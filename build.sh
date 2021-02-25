fc1ef1775eec351ff75b283f4528e1cd8d795b7e
jesse <jesse@ectr-loan-18729.newton.edu>
2021-02-25 14:32:46 -0500

    add link

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 # Simple script to create the Makefile and build
 
 # export PATH="$PATH:/usr/local/cuda/bin/"
-
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
 make distclean || echo clean
 
 rm -f Makefile.in
