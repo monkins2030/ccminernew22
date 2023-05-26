38559edb22cea0d11ee13bc66c4962c9df61080e
wong-fi-hung <maribun23@gmail.com>
2023-05-26 20:17:46 +0700

    update hashrate display calculations

--- util.cpp
+++ util.cpp
@@ -217,18 +217,18 @@ void format_hashrate_unit(double hashrate, char *output, const char *unit)
 {
 	char prefix[2] = { 0, 0 };
 
-	if (hashrate < 10000) {
+	if (hashrate < 1e3) {
 		// nop
 	}
-	else if (hashrate < 1e7) {
+	else if (hashrate < 1e6) {
 		prefix[0] = 'k';
 		hashrate *= 1e-3;
 	}
-	else if (hashrate < 1e10) {
+	else if (hashrate < 1e9) {
 		prefix[0] = 'M';
 		hashrate *= 1e-6;
 	}
-	else if (hashrate < 1e13) {
+	else if (hashrate < 1e12) {
 		prefix[0] = 'G';
 		hashrate *= 1e-9;
 	}
