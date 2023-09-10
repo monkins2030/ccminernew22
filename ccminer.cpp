e0b09f29916a2938766d0f4f2ecfb221a2ce947e
PSLLSP <6446102+PSLLSP@users.noreply.github.com>
2023-09-10 19:06:01 +0200

    fix call of method client.reconnect()

--- ccminer.cpp
+++ ccminer.cpp
@@ -2606,7 +2606,7 @@ wait_stratum_url:
 			pthread_mutex_unlock(&g_work_lock);
 			restart_threads();
 
-			if (!stratum_connect(&stratum, pool->url) ||
+			if (!stratum_connect(&stratum, stratum.url) ||
 			    !stratum_subscribe(&stratum) ||
 			    !stratum_authorize(&stratum, pool->user, pool->pass))
 			{
