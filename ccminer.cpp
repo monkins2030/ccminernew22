0dd82775dff128d4dcbb4d0eb814d6e38cdbbd30
PSLLSP <6446102+PSLLSP@users.noreply.github.com>
2023-12-10 10:52:05 +0100

    Clear() is not needed anymore, removed

--- ccminer.cpp
+++ ccminer.cpp
@@ -3421,18 +3421,6 @@ BOOL WINAPI ConsoleHandler(DWORD dwType)
 }
 #endif
 
-void Clear()
-{
-#if defined _WIN32
-	system("cls");
-#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-	system("clear");
-#elif defined (__APPLE__)
-	system("clear");
-#endif
-}
-
-
 int main(int argc, char *argv[])
 {
 	struct thr_info *thr;
